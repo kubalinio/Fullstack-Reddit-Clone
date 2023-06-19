c16629b78924ae9a60cd75bda26ad1200cbc886d
Jakub <jsapalawebdev@gmail.com>
2023-06-19 14:01:38 +0200

    Build all new community creating logic (page, api, prisma, React Query, custom toast)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Open_Sans } from 'next/font/google'
 import { cn } from '@/lib/utils'
 import { Navbar } from '@/components/Navbar'
 import { Toaster } from '@/components/ui/Toaster'
+import { Providers } from '@/components/Providers'
 
 export const metadata = {
   title: 'Needit',
@@ -13,16 +14,18 @@ const OpenSans = Open_Sans({ subsets: ['latin'] })
 
 export default function RootLayout({ children, authModal }: { children: React.ReactNode; authModal: React.ReactNode }) {
   return (
-    <html lang="en" className={cn('light bg-white text-slate-900 antialiased', OpenSans)}>
+    <html lang="en" className={cn('light bg-white text-slate-900 antialiased', OpenSans.className)}>
       <body className="min-h-screen bg-slate-50 pt-12 antialiased">
-        {/* @ts-expect-error Server Component */}
-        <Navbar />
+        <Providers>
+          {/* @ts-expect-error Server Component */}
+          <Navbar />
 
-        {authModal}
+          {authModal}
 
-        <div className="container mx-auto h-full max-w-7xl pt-12">{children}</div>
+          <div className="container mx-auto h-full max-w-7xl pt-12">{children}</div>
 
-        <Toaster />
+          <Toaster />
+        </Providers>
       </body>
     </html>
   )
